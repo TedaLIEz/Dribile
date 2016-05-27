cc1793d5ccb908bd927b5af96be4de849bd0d4f5
TedaLIEz <aliezted@gmail.com>
2016-05-27 21:07:14 +0800

    AboutPage

---
+++ app/src/main/java/com/hustunique/jianguo/dribile/ui/activity/AboutActivity.java
@@ -0,0 +1,41 @@
+package com.hustunique.jianguo.dribile.ui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.hustunique.jianguo.dribile.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
+public class AboutActivity extends BaseActivity {
+
+    @Bind(R.id.container)
+    FrameLayout aboutLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+        Element versionElement = new Element();
+        versionElement.setTitle("Version 0.1");
+        Element changeLog = new Element();
+        changeLog.setTitle("Changelog");
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setDescription("Simple Application using dribbble api")
+                .setImage(R.mipmap.ic_launcher)
+                .addItem(versionElement)
+                .addItem(changeLog)
+                .addGroup("Connect to us")
+                .addEmail("aliezted@gmail.com")
+                .addWebsite("http://tedaliez.github.io")
+                .addTwitter("TedaLIEz")
+                .addGitHub("TedaLIEz")
+                .create();
+        aboutLayout.addView(aboutPage);
+    }
+}
--- app/src/main/java/com/hustunique/jianguo/dribile/views/SettingView.java
+++ app/src/main/java/com/hustunique/jianguo/dribile/views/SettingView.java
@@ -1,10 +1,16 @@
 package com.hustunique.jianguo.dribile.views;
 
+import android.content.Intent;
+
 /**
  * Created by JianGuo on 5/26/16.
  */
 public interface SettingView {
     void onClearSuccess();
-    void onClearFailed();
+    void onClearFailed(Exception e);
     void logout();
+
+    void sendEmailIntent(Intent emailIntent);
+
+    void about();
 }
